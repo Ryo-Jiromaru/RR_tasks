b968c2ce635f8b3baf019581531e99d057c7bec0
Ryo-Jiromaru <rjmaru1104@gmail.com>
2022-03-18 13:04:49 +0900

    createアクションの作成

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -7,4 +7,18 @@ class Api::V1::TodosController < ApplicationController
         render json: genres, include: [:todos]
     end
 
+    def create
+        todo = Todo.new(todo_params)
+        if todo.save
+            render json: todo
+        else
+            render json: todo.errors, status: 422
+        end
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:about, :genre_id)
+    end
+
 end
