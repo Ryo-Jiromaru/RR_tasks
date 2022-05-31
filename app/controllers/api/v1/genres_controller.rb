2cc681401514ff69abb138641b1153d98146325e
Ryo-Jiromaru <rjmaru1104@gmail.com>
2022-05-31 23:04:05 +0900

    genreを追加可能に

---
+++ app/controllers/api/v1/genres_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::GenresController < ApplicationController
+    def create
+        genre = Genre.new(genre_params)
+        if genre.save
+            render json: genre
+        else
+            render json: genre.errors, status: 422
+        end
+    end
+
+    private
+    def genre_params
+        params.require(:genre).permit(:name)
+    end
+end
